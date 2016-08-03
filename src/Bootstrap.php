079459188b037c98a5dda5bf36f6c70ee5c967a6
MartkCz <antik494@gmail.com>
2016-08-03 11:36:47 +0200

    Registration of SoftExtensionsExtension moved to Bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,40 +3,39 @@
 namespace Thunbolt;
 
 use Nette\Configurator;
-use Thunbolt\Errors\ErrorTemplate;
-use Tracy\Debugger;
+use Thunbolt\SoftExtensions\DI\SoftExtensionsExtension;
 
 class Bootstrap {
 
 	/** @var string */
 	private $baseDir;
 
+	/** @var Configurator */
+	private $configurator;
+
 	/**
 	 * @param string $baseDir
 	 */
-	public function __construct($baseDir) {
+	public function __construct($baseDir, Configurator $configurator) {
 		$this->baseDir = $baseDir;
+		$this->configurator = $configurator;
 	}
 
-	/**
-	 * @param Configurator $configurator
-	 */
-	public function loadComposerConfig(Configurator $configurator) {
-		$configFile = $this->baseDir . '/../mod/composer.json';
+	public function loadComposerConfig($path = NULL) {
+		$configFile = $path ? : $this->baseDir . '/../mod/composer.json';
 		if (file_exists($configFile)) {
 			$configs = json_decode(file_get_contents($configFile), TRUE);
 			if (isset($configs['configs']) && is_array($configs['configs'])) {
 				foreach ($configs['configs'] as $config) {
-					$configurator->addConfig($this->baseDir . '/../' . $config);
+					$this->configurator->addConfig($this->baseDir . '/../' . $config);
 				}
 			}
 		}
 	}
 
 	public function initialize() {
-		// Own error template
-		if (!Debugger::$errorTemplate && class_exists('Thunbolt\Errors\ErrorTemplate')) {
-			Debugger::$errorTemplate =  ErrorTemplate::getErrorTemplate();
+		if (class_exists(SoftExtensionsExtension::class)) {
+			SoftExtensionsExtension::register($this->configurator);
 		}
 	}
 
