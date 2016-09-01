f51a168e619505e2284624dbd95417b3c5e3f46b
MartkCz <antik494@gmail.com>
2016-09-01 11:35:23 +0200

    Updated Bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,6 +3,8 @@
 namespace Thunbolt;
 
 use Nette\Configurator;
+use Thunbolt\Bundles\DI\BundlesExtension;
+use Thunbolt\Composer\ComposerDirectories;
 use Thunbolt\SoftExtensions\DI\SoftExtensionsExtension;
 
 class Bootstrap {
@@ -15,21 +17,20 @@ class Bootstrap {
 
 	/**
 	 * @param string $baseDir
+	 * @param Configurator $configurator
+	 * @param bool $initDirs
 	 */
-	public function __construct($baseDir, Configurator $configurator) {
+	public function __construct($baseDir, Configurator $configurator, $initDirs = TRUE) {
 		$this->baseDir = $baseDir;
 		$this->configurator = $configurator;
-	}
-
-	public function loadComposerConfig($path = NULL) {
-		$configFile = $path ? : $this->baseDir . '/../mod/composer.json';
-		if (file_exists($configFile)) {
-			$configs = json_decode(file_get_contents($configFile), TRUE);
-			if (isset($configs['configs']) && is_array($configs['configs'])) {
-				foreach ($configs['configs'] as $config) {
-					$this->configurator->addConfig($this->baseDir . '/../' . $config);
-				}
-			}
+		if ($initDirs) {
+			$this->configurator->addParameters([
+				'bundlesDir' => $baseDir . '/bundles',
+				'layoutsDir' => $baseDir . '/layouts',
+				'transDir' => $baseDir . '/translations',
+				'plgDir' => $baseDir . '/../' . ComposerDirectories::PLG_DIR,
+				'plgResDir' => $baseDir . '/../' . ComposerDirectories::PLG_RES_DIR,
+			]);
 		}
 	}
 
@@ -37,6 +38,15 @@ class Bootstrap {
 		if (class_exists(SoftExtensionsExtension::class)) {
 			SoftExtensionsExtension::register($this->configurator);
 		}
+		BundlesExtension::register($this->configurator);
+
+		$this->configurator->enableDebugger($this->baseDir . '/../var/log');
+		$this->configurator->setTempDirectory($this->baseDir . '/../var/tmp');
+	}
+
+	public function registerCoreConfigs() {
+		$this->configurator->addConfig($this->baseDir . '/config/core/extensions.neon');
+		$this->configurator->addConfig($this->baseDir . '/config/core/config.neon');
 	}
 
 }
